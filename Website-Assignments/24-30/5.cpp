e6cdca0046fbaf42238c45dddecddf08646eb2d7
Ali Eldeba <alieldeba375@gmail.com>
2023-01-27 11:42:38 +0200

    Create 5.cpp

---
+++ Website-Assignments/24-30/5.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Assign The Values
+    char a = NULL;
+    char b = 'd';
+    char c = '\n';
+    
+    // Do Not Edit
+    cout << b * c + a << "\n"; // 1000
+
+    return 0;
+}
