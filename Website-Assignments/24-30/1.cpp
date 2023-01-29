c275f821df0d5bd765447d47c00ddd75b7a67a9f
Ali Eldeba <alieldeba375@gmail.com>
2023-01-29 12:14:04 +0200

    Update 1.cpp

--- Website-Assignments/24-30/1.cpp
+++ Website-Assignments/24-30/1.cpp
@@ -5,7 +5,7 @@ int main() {
     int num = 3;
     
     cout << num + num << "\n";                 // 6
-    cout << num ? num ? num << "\n";           // 6
+    cout << num - num * num << "\n";           // 6
     cout << (num * num) - num << "\n";         // 6
     cout << (num + num + num) - num << "\n";   // 6
     cout << (num - num) + (num + num) << "\n"; // 6
