c3c73ac0356f0c4d3da61ee0ff3435afc8a4abb3
Ali Eldeba <alieldeba375@gmail.com>
2023-01-30 17:43:54 +0200

    Update 5.cpp

--- Website-Assignments/16-23/5.cpp
+++ Website-Assignments/16-23/5.cpp
@@ -9,13 +9,13 @@ int main() {
     
     // Output Needed
     // "ASCII Value of ~ Is 126"
-    cout << "ASCII Value of " << a << " Is " << a - 0 << endl;
+    cout << "ASCII Value of " << a << " Is " << int(a) << endl;
     // "ASCII Value of & Is 38"
-    cout << "ASCII Value of " << b << " Is " << b - 0 << endl;
+    cout << "ASCII Value of " << b << " Is " << int(b) << endl;
     // "ASCII Value of % Is 37"
-    cout << "ASCII Value of " << c << " Is " << c - 0 << endl;
+    cout << "ASCII Value of " << c << " Is " << int(c) << endl;
     // "ASCII Value of A Is 65"
-    cout << "ASCII Value of " << d << " Is " << d - 0 << endl;
+    cout << "ASCII Value of " << d << " Is " << int(d) << endl;
 
     return 0;
 }
