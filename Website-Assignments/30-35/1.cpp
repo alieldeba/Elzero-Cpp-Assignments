841688c687a96252270e21dd55ace6a8760e2aca
Ali Eldeba <alieldeba375@gmail.com>
2023-02-15 11:55:33 +0200

    Create 1.cpp

---
+++ Website-Assignments/30-35/1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+int age = 40;
+int points = 800;
+float rate = 8.5f;
+
+// Your Condition Here
+if (age > 18 && points > 500 && rate > 5) 
+    cout << "Yes Age > 18 & Points > 500 & Rate > 5";
+
+// Output Needed If All Conditions Is True
+// "Yes Age > 18 & Points > 500 & Rate > 5"
+
+    return 0;
+}
