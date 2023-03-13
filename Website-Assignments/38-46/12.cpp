cbd1eb3628c8b7aae0cc240429efd67093ded8d3
Ali Eldeba <alieldeba375@gmail.com>
2023-03-13 23:13:07 +0200

    Create

---
+++ Website-Assignments/38-46/12.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Do Not Edit The Next 6 Lines
+    int nums[] = {1, 2, 3, 5};
+    int vals[] = {10, 20, 40, 30, 10, 60};
+    int i1 = nums[3];
+    int i2 = nums[0];
+    int i3 = nums[1];
+    int i4 = nums[2];
+    
+    // Need To Get Ouput 150
+    // You Can Use Plus Operator Only
+    
+    // Your Code Here
+    cout << vals[i1] + vals[i2] + vals[i3] + vals[i4];
+
+    return 0;
+}
