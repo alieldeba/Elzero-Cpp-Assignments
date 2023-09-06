77bc34fb652d82d50b89599ce650b3e502330e47
Ali Eldeba <alieldeba375@gmail.com>
2023-09-06 13:13:02 +0300

    Update 1.cpp

--- Website-Assignments/24-29/1.cpp
+++ Website-Assignments/24-29/1.cpp
@@ -4,13 +4,13 @@ using namespace std;
 int main() {
     int num = 3;
     
-    cout << num + num << "\n";                 // 6
-    cout << num - num * num << "\n";           // 6
-    cout << (num * num) - num << "\n";         // 6
-    cout << (num + num + num) - num << "\n";   // 6
-    cout << (num - num) + (num + num) << "\n"; // 6
+    cout << num + num << "\n";                  // 6
+    cout << (num + num) + (num % num) << "\n";  // 6 
+    cout << (num * num) - num << "\n";          // 6
+    cout << (num + num + num) - num << "\n";    // 6
+    cout << (num - num) + (num + num) << "\n";  // 6
     cout << (num == num) * (num + num) << "\n"; // 6
-    cout << (num > num) + (num + num) << "\n"; // 6
+    cout << (num > num) + (num + num) << "\n";  // 6
 
     return 0;
 }
